c58373c24eac38e33031663585cd09207d4d0d87
Nuttanun12 <nongnutza12@gmail.com>
2021-08-19 17:54:11 +0700

    speech to text
    Add spech to text module

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from tkinter import *
 root = Tk()
 root.option_add('font', 'consulor 26')
 root.title("ToDoList")
-root.geometry("400x400")
+root.geometry("400x250")
 
-todo = Listbox(root, height=24, width=40)
-todo.place(x=150,y=4)
+Button(root, text="list add",padx=60,).place(x=8,y=80)
+
+todo = Listbox(root, height=13, selectmode='SINGLE')
+todo.place(x=250,y=4)
+
 
 mainloop()
