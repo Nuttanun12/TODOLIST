9f2ca0ce87655284c2de556045e4436eb51a9ee2
Nuttanun12 <nongnutza12@gmail.com>
2021-08-19 22:05:24 +0700

    Add buttom

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ from tkinter import *
 root = Tk()
 root.option_add('font', 'consulor 26')
 root.title("ToDoList")
-root.geometry("400x250")
+root.geometry("350x250")
 
-Button(root, text="list add",padx=60,).place(x=8,y=80)
+Label(root, text="List name :").place(x=20, y=30)
+Button(root, text="Add list",width=20).place(x=20,y=90)
+Button(root, text="Remove list",width=20).place(x=20,y=120)
+Button(root, text="Clear list",width=20).place(x=20,y=150)
+Button(root, text="Exit",width=20).place(x=20,y=180)
 
-todo = Listbox(root, height=13, selectmode='SINGLE')
-todo.place(x=250,y=4)
-
+listname = Entry(root, width=25)
+todo = Listbox(root, height=12, width=25,selectmode='SINGLE')
+todo.place(x=180, y=23)
+listname.place(x=20, y=50)
 
 mainloop()
