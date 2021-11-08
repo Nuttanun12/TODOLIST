de1761b296e8f5e92b2c13471fc86570c9a4c3a9
Nuttanun12 <nongnutza12@gmail.com>
2021-11-08 11:50:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,49 @@
 from tkinter import *
+import speech_recognition as sr
 
 root = Tk()
+todolist = []
 root.option_add('font', 'consulor 26')
 root.title("ToDoList")
-root.geometry("350x250")
+root.geometry("420x280")
+r = sr.Recognizer()
 
-photo = PhotoImage(file="D:\System\Desktop\งาน\todolist app\Photo\microphone_icon.png")
-photoimage = photo.subsample(1,1)
-Button(root, image= photoimage).place(x=0, y=0)
+photo = PhotoImage(file= r"D:\System\Desktop\work\todolist_app\TODOLIST\Photo\microphone.png")
+photoimage = photo.subsample(6,6)
+
 Label(root, text="List name :").place(x=20, y=30)
-Button(root, text="Add list",width=20).place(x=20,y=90)
-Button(root, text="Remove list",width=20).place(x=20,y=120)
-Button(root, text="Clear list",width=20).place(x=20,y=150)
 Button(root, text="Exit",width=20).place(x=20,y=180)
 
-listname = Entry(root, width=25)
-todo = Listbox(root, height=12, width=25,selectmode='SINGLE')
-todo.place(x=180, y=23)
+listname = Entry(root, width=16, font=('Tahoma', 12))
 listname.place(x=20, y=50)
+todo = Listbox(root, height=12, width=25, font=('Tahoma'))
+todo.place(x=180, y=23)
 
-mainloop()
+def addlist():
+    text = listname.get()
+    if len(text) >= 1:
+        todo.insert(END, text)
+        listname.delete(0, END)
+    else:
+        pass
+    return NONE
+
+def speechtotext():
+    with sr.Microphone() as source:
+        audio_text = r.listen(source)
+        try:
+            todo.insert(END, r.recognize_google(audio_text, language='th'))
+        except:
+            pass
+
+def clearlist():
+    todo.delete(0, END)
+
+
+
+Button(root, text="Add list" ,command=addlist ,width=20).place(x=20,y=90)
+Button(root, image= photoimage,command=speechtotext).place(x=20, y=212)
+Button(root, text="Clear list",width=20, command=clearlist).place(x=20,y=150)
+Button(root, text="Remove list",width=20).place(x=20,y=120)
+
+root.mainloop()
