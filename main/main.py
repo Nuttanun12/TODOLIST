d491cc0cbaa8b2a1dc07afeb818cfa1b3139b7cb
Nuttanun12 <nongnutza12@gmail.com>
2021-12-03 11:30:38 +0700

    Done
    เขียน DB จาก SQL
    สร้าง table
    updatelitst func
    delete func
    exit func
    retask from db
    add title on db
    clear db

--- main/main.py
+++ main/main.py
@@ -2,8 +2,9 @@ from tkinter import *
 import speech_recognition as sr
 import sqlite3 as sq
 
-conn = sq.connect('listname.db')
+conn = sq.connect("list.db")
 cur = conn.cursor()
+cur.execute('CREATE TABLE IF NOT EXISTS tasks (title text)')
 
 root = Tk()
 todolist = []
@@ -21,29 +22,66 @@ def speechtotext():
     with sr.Microphone() as source:
         audio_text = r.listen(source)
         try:
-            todo.insert(END, r.recognize_google(audio_text, language='th'))
+            text = r.recognize_google(audio_text, language='th')
+            todo.insert(END, text)
+            cur.execute('insert into tasks values (?)',(text,))
+            todolist.append(text)
         except:
             pass
 
 def addlist():
-    text = listname.get()
-    if len(text) >= 1:
-        todo.insert(END, text)
+    word = listname.get()
+    if len(word) >= 1:
+        todo.insert(END, word)
         listname.delete(0, END)
+        todolist.append(word)
+        cur.execute('INSERT INTO tasks VALUES (?)',(word,))
     else:
         pass
     return NONE
 
+def updatelist():
+    todo.delete(0,END)
+    for i in todolist:
+        todo.insert(END, i)
+
+def delete():
+    try:
+        selete = todo.get(todo.curselection())
+        if selete in todolist:
+            todolist.remove(selete)
+            updatelist()
+            cur.execute('delete from tasks where title = ?',(selete))
+            print(selete)
+    except:
+        pass
+
+
 def clearlist():
     todo.delete(0, END)
+    cur.execute('delete from tasks')
+    todolist.clear()
+
+def exit():
+    root.destroy()
+
+def retasks():
+    for row in conn.execute("select title from tasks"):
+        todo.insert(END, row)
+        todolist.append(row)
 
 photo = PhotoImage(file= r"D:\System\Desktop\work\todolist_app\TODOLIST\Photo\microphone.png")
 photoimage = photo.subsample(6,6)
 Button(root, text="Add list" ,command=addlist ,width=20).place(x=20,y=90)
 Button(root, image= photoimage,command=speechtotext).place(x=20, y=212)
 Button(root, text="Clear list",width=20, command=clearlist).place(x=20,y=150)
-Button(root, text="Remove list",width=20).place(x=20,y=120)
+Button(root, text="Remove list",width=20, command=delete).place(x=20,y=120)
 Label(root, text="List name :").place(x=20, y=30)
-Button(root, text="Exit",width=20).place(x=20,y=180)
+Button(root, text="Exit",width=20, command=exit).place(x=20,y=180)
+
+retasks()
 
 root.mainloop()
+
+conn.commit()
+cur.close()
