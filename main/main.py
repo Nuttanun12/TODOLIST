3a3f370083b793d0e567fdff5c028f104779ac0a
Nuttanun12 <nongnutza12@gmail.com>
2022-01-10 21:52:28 +0700

    add tread, auto add speech to text on list by voice

--- main/main.py
+++ main/main.py
@@ -1,35 +1,48 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import messagebox
+from tkinter import Listbox
+from tkinter import Entry
+from tkinter import END
+from tkinter import Button
+from tkinter import Label
+from tkinter import PhotoImage
+from tkinter.constants import DISABLED, NORMAL
 import speech_recognition as sr
 import sqlite3 as sq
+import threading
+import time
 
 conn = sq.connect("list.db")
 cur = conn.cursor()
 cur.execute('CREATE TABLE IF NOT EXISTS tasks (title text)')
 
-root = Tk()
+root = tk.Tk()
 todolist = []
 root.option_add('font', 'consulor 26')
 root.title("ToDoList")
 root.geometry("420x280")
 r = sr.Recognizer()
+auto = False
 
 listname = Entry(root, width=16, font=('Tahoma', 12))
 listname.place(x=20, y=50)
 todo = Listbox(root, height=12, width=25, font=('Tahoma'))
 todo.place(x=180, y=23)
 
-def speechtotext():
-    with sr.Microphone() as source:
-        audio_text = r.listen(source)
+def speech_thread():
+    def speechtotext():
+        with sr.Microphone() as source:
+          audio_text = r.listen(source)
         try:
-            text = r.recognize_google(audio_text, language='th')
-            todo.insert(END, text)
-            cur.execute('insert into tasks values (?)',(text,))
-            todolist.append(text)
-            conn.commit()
+              text = r.recognize_google(audio_text, language='th')
+              todo.insert(END, text)
+              cur.execute('insert into tasks values (?)',(text,))
+              todolist.append(text)
+              conn.commit()
         except:
             pass
+    threadspeech = threading.Thread(target=speechtotext)
+    threadspeech.start()
 
 def addlist():
     word = listname.get()
@@ -41,7 +54,6 @@ def addlist():
         conn.commit()
     else:
         pass
-    return NONE
 
 def updatelist():
     todo.delete(0,END)
@@ -79,18 +91,56 @@ def retasks():
         todo.insert(END, row)
         todolist.append(row)
 
+def listen_thread():
+    global auto
+    auto = False
+    b1['state'] = DISABLED
+    b2['state'] = NORMAL
+    def autospeechtotext():
+        while True:
+            try:
+                with sr.Microphone() as source:
+                    audio_text = r.listen(source)
+                    text = r.recognize_google(audio_text, language='th')
+                    print(text)
+                    global auto
+                    if auto:
+                        break
+                    if "ปิด" in text:
+                        break
+                    if "เพิ่ม" in text:
+                        print("kanungnij")
+                    
+            except:
+                pass
+        b1['state'] = NORMAL
+        b2['state'] = DISABLED
+    threadauto = threading.Thread(target=autospeechtotext)
+    threadauto.start()
+
+def changer():
+    global auto
+    auto = True
+
+    
+
 photo = PhotoImage(file= r"microphone.png")
 photoimage = photo.subsample(6,6)
 Button(root, text="Add list" ,command=addlist ,width=20).place(x=20,y=90)
-Button(root, image= photoimage,command=speechtotext).place(x=20, y=212)
+Button(root, image= photoimage,command=speech_thread).place(x=20, y=212)
 Button(root, text="Clear list",width=20, command=clearlist).place(x=20,y=150)
 Button(root, text="Remove list",width=20, command=delete).place(x=20,y=120)
 Label(root, text="List name :").place(x=20, y=30)
 Button(root, text="Exit",width=20, command=exit).place(x=20,y=180)
+b1 = Button(root, text='ON', width=5,command=listen_thread)
+b2 = Button(root, text='OFF', width=5,command=changer,state=DISABLED)
+b1.place(x=70, y=220)
+b2.place(x=120, y=220)
 
 retasks()
 
 root.mainloop()
 
+auto = True
 conn.commit()
 cur.close()
